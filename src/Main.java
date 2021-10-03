fa55198696921045fa8fa7a45ffd02012dba7bb2
Mahdi-Jhr <jahanshir.sm92@gmail.com>
2021-10-03 11:09:29 +0330

    add a new object of TaxiOnline and run method showMenu

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.text.ParseException;
 
 public class Main {
     public static void main(String[] args){
-
-
+TaxiOnline taxiOnline=new TaxiOnline();
+taxiOnline.showMenu();
     }
 }
--- src/TaxiOnline.java
+++ src/TaxiOnline.java
@@ -1,2 +1,12 @@
 public class TaxiOnline {
+    public void showMenu(){
+        System.out.println("---------- welcome to taxi online ----------");
+        System.out.println("1.Add a group of drivers" +
+                "\n2.Add a group of passengers" +
+                "\n3.Driver signup or login" +
+                "\n4.Passenger signup or login" +
+                "\n5.show ongoing travels" +
+                "\n6.show a list of drivers" +
+                "\n7.show a list of passengers\n ------------------------------");
+    }
 }
