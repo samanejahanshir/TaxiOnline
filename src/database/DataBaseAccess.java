1940a5e79da8e83eb1bb34e8463c3810e55776b9
unknown <jahanshir.sm92@gmail.com>
2021-12-15 17:54:33 +0330

    add Session Factory in DataBaseAccess class

--- src/database/DataBaseAccess.java
+++ src/database/DataBaseAccess.java
@@ -1,17 +1,27 @@
 package database;
 
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseAccess {
     private Connection connection=null;
+    private static SessionFactory sessionFactory=null;
 
     public DataBaseAccess() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_online_database", "root", "1234567890");
     }
-
+public  static SessionFactory getSessionFactory(){
+        if(sessionFactory==null){
+            sessionFactory=new Configuration().configure("hibernate/hibernate.cfg.xml").buildSessionFactory();
+        }
+        return  sessionFactory;
+}
     public Connection getConnection() {
         return connection;
     }
