d288b3092cae9655ed4eee0d86e4d92e8eedb3b0
unknown <jahanshir.sm92@gmail.com>
2021-12-15 17:55:42 +0330

    fix methods and use hibernate in class DriverDataBase

--- src/database/DriverDataBase.java
+++ src/database/DriverDataBase.java
@@ -1,10 +1,16 @@
 package database;
 
+import database.DataBaseAccess;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import other_class.Travel;
 import other_class.Vehicle;
 import person.Driver;
 import person.Passenger;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -17,7 +23,7 @@ public class DriverDataBase extends DataBaseAccess {
     }
 
     public int save(Driver driver) throws SQLException {
-        if (getConnection() != null) {
+       /* if (getConnection() != null) {
             Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("INSERT INTO driver (first_name , last_name , national_code , mobile_phone , gender , birth_date , car_tag , balance , origin) VALUES ('%s','%s','%s','%s',%b,'%s','%s',%f,'%s')",
                     driver.getFirstName(), driver.getLastName(), driver.getNationalCode(), driver.getPhoneNumber(), driver.isGender(), driver.getBirthDate(), driver.getCarTag(), driver.getBalance(), driver.getOrigin());
@@ -25,9 +31,15 @@ public class DriverDataBase extends DataBaseAccess {
             return i;
         } else {
             return 0;
-        }
+        }*/
+        Session session = DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        int id = (int) session.save(driver);
+        transaction.commit();
+        session.close();
+        return id;
     }
-    public int updateDriverStatus(Driver driver) throws SQLException {
+   /* public int updateDriverStatus(Driver driver) throws SQLException {
         if (getConnection() != null) {
             Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("UPDATE driver SET status = %b WHERE id_driver=%d", driver.getStatus(), driver.getId());
@@ -39,12 +51,18 @@ public class DriverDataBase extends DataBaseAccess {
             }
 
         }
-        return -1;
+        return -1;*//*
+        Session session = DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(driver);
+        transaction.commit();
+        session.close();
+        return 1;
 
-    }
+    }*/
 
     public int updateDriver(Driver driver) throws SQLException {
-        if (getConnection() != null) {
+        /*if (getConnection() != null) {
             Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("UPDATE driver SET origin = '%s' WHERE national_code='%s'", driver.getOrigin(), driver.getNationalCode());
             int i = statement.executeUpdate(sqlQuery);
@@ -55,11 +73,17 @@ public class DriverDataBase extends DataBaseAccess {
             }
 
         }
-        return -1;
+        return -1;*/
+        Session session = DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(driver);
+        transaction.commit();
+        session.close();
+        return 1;
     }
 
-    public int searchDriver(String national_code) throws SQLException {
-        if (getConnection() != null) {
+    public Driver searchDriver(String national_code) throws SQLException {
+       /* if (getConnection() != null) {
             Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("SELECT id_driver from driver WHERE national_code='%s'", national_code);
             ResultSet resultSet = statement.executeQuery(sqlQuery);
@@ -70,12 +94,40 @@ public class DriverDataBase extends DataBaseAccess {
             }
         } else {
             return -1;
-        }
+        }*/
+       /* Session session = DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Driver> criteria = builder.createQuery(Driver.class);
+        criteria.from(Passenger.class);
+        List<Driver> driverList = session.createQuery(criteria).getResultList();
+        Driver driver = driverList.stream().filter(passenger1 -> passenger1.getNationalCode().equalsIgnoreCase(national_code)).findAny().get();
+        System.out.println(driver);
+        transaction.commit();
+        session.close();
+        return driver;*/
+        Session session=DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction=session.beginTransaction();
+        Query<Driver> query = session.createQuery("from Driver d where d.nationalCode=:nationalCode", Driver.class);
+        query.setParameter("nationalCode",national_code);
+        List<Driver> resultList = query.getResultList();
+        transaction.commit();
+        session.close();
+        return resultList.get(0);
     }
-
+    public Driver searchDriverById(int id) throws SQLException {
+        Session session=DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction=session.beginTransaction();
+        Query<Driver> query = session.createQuery("from Driver d where d.id=:id", Driver.class);
+        query.setParameter("id",id);
+        List<Driver> resultList = query.getResultList();
+        transaction.commit();
+        session.close();
+        return resultList.get(0);
+    }
     public List<Driver> getListDrivers() throws SQLException {
-        List<Driver> drivers = new ArrayList<>();
-        if (getConnection() != null) {
+        List<Driver> drivers ;
+        /*if (getConnection() != null) {
             Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("SELECT * FROM driver");
             ResultSet resultSet = statement.executeQuery(sqlQuery);
@@ -87,7 +139,21 @@ public class DriverDataBase extends DataBaseAccess {
             }
             return drivers;
         }
-        return null;
+        return null;*/
+       /* Session session = DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Driver> criteria = builder.createQuery(Driver.class);
+        criteria.from(Driver.class);
+        drivers = session.createQuery(criteria).getResultList();
+        return drivers;*/
+        Session session=DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction=session.beginTransaction();
+        Query<Driver> query = session.createQuery("from Driver", Driver.class);
+        List<Driver> resultList = query.getResultList();
+        transaction.commit();
+        session.close();
+        return resultList;
     }
 
     public double showBalance(String nationalCode) throws SQLException {
@@ -106,7 +172,7 @@ public class DriverDataBase extends DataBaseAccess {
     }
 
     public int saveVehicle(Vehicle vehicle) throws SQLException {
-        if (getConnection() != null) {
+       /* if (getConnection() != null) {
             Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("INSERT INTO vehicle (vehicle_tag , type , model , color) VALUES ('%s','%s','%s','%s')",
                     vehicle.getVehicleTag(), vehicle.getType(), vehicle.getModel(), vehicle.getColor());
@@ -114,12 +180,18 @@ public class DriverDataBase extends DataBaseAccess {
             return i;
         } else {
             return 0;
-        }
+        }*/
+        Session session = DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        int id = (int) session.save(vehicle);
+        transaction.commit();
+        session.close();
+        return id;
     }
 
     public Vehicle getVehicle(String carTag) throws SQLException {
 
-        if (getConnection() != null) {
+       /* if (getConnection() != null) {
             Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("SELECT * FROM vehicle WHERE vehicle_tag='%s'", carTag);
             ResultSet resultSet = statement.executeQuery(sqlQuery);
@@ -129,11 +201,30 @@ public class DriverDataBase extends DataBaseAccess {
                 return vehicle;
             }
         }
-        return null;
+        return null;*/
+      /*  Session session = DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Vehicle> criteria = builder.createQuery(Vehicle.class);
+        criteria.from(Vehicle.class);
+        List<Vehicle> passengerList = session.createQuery(criteria).getResultList();
+        Vehicle vehicle = passengerList.stream().filter(vehicle1 -> vehicle1.getVehicleTag().equalsIgnoreCase(carTag)).findAny().get();
+        System.out.println(vehicle);
+        transaction.commit();
+        session.close();
+        return vehicle;*/
+        Session session=DataBaseAccess.getSessionFactory().openSession();
+        Transaction transaction=session.beginTransaction();
+        Query<Vehicle> query = session.createQuery("from Vehicle d where d.vehicleTag=:carTag",Vehicle.class);
+        query.setParameter("carTag",carTag);
+        List<Vehicle> resultList = query.getResultList();
+        transaction.commit();
+        session.close();
+        return resultList.get(0);
     }
 
     public boolean changeBalance(String national_code, double balance) throws SQLException {
-        if (getConnection() != null) {
+       /* if (getConnection() != null) {
             Statement statement = getConnection().createStatement();
             String sqlQuery = String.format("UPDATE driver SET balance=%2f WHERE national_code='%s'", balance, national_code);
             int i = statement.executeUpdate(sqlQuery);
@@ -144,7 +235,11 @@ public class DriverDataBase extends DataBaseAccess {
             }
         } else {
             return false;
-        }
+        }*/
+        Driver driver=searchDriver(national_code);
+        driver.setBalance(balance);
+        updateDriver(driver);
+        return  true;
     }
 
 }
